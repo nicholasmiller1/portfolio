a4e05e25541dc0f57a62cffc48ff437da90ba7a5
Nicholas Miller <nicholascmiller123@gmail.com>
2021-08-13 23:57:53 -0400

    Small changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,10 +8,10 @@ const Footer = (props) => {
         <div className="footer" id="contact">
             <div className="socials">
                 <a href="mailto:nicholascmiller123@gmail.com"><HiMail/></a>
-                <a href="https://www.linkedin.com/in/nicholas-c-miller-14a088205/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
+                <a href="https://www.linkedin.com/in/nicholas-c-miller/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
                 <a href="https://github.com/nicholasmiller1" target="_blank" rel="noreferrer"><FaGithub/></a>
             </div>
-            <a href="https://drive.google.com/file/d/1FLSFRMmGmESRbvfNR7bGqH7QdB4xVzAB/view?usp=sharing" target="_blank" rel="noreferrer" id="footer-resume">Resume</a>
+            <a href="https://drive.google.com/file/d/1RoF3UvPp_83mQWJW85vVBbHlYI4nE0xJ/view?usp=sharing" target="_blank" rel="noreferrer" id="footer-resume">Resume</a>
         </div>
     );
 }
